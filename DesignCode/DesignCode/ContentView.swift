afe9b7c181ece12c271b947730bc1729554edb32
xueming liu <liuxueming@ozi-tech.cn>
2020-08-25 17:31:22 +0800

    config UI

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -2,20 +2,162 @@
 //  ContentView.swift
 //  DesignCode
 //
-//  Created by xueming liu on 2020/8/25.
+//  Created by xueming liu on 2020/8/24.
 //  Copyright © 2020 xueming liu. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            CardBottomView()
+            .blur(radius: show ? 20 : 0)
+                .animation(.easeInOut(duration: 0.3))
+            
+            
+            CardView()
+                .background(show ? Color.red : Color.green)
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 50 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.easeInOut(duration: 0.5))
+                .blendMode(.hardLight)
+                .offset(x: viewState.width, y: viewState.height)
+            CardView()
+                .background(show ? Color.yellow : Color.blue)
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.90)
+                .rotationEffect(Angle(degrees: show ? 10 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 40 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.spring())
+                .blendMode(.hardLight)
+                .offset(x: viewState.width, y: viewState.height)
+            
+            CertificateView()
+                .offset(x: viewState.width, y: viewState.height)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: show ? 5 : 0))
+                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.easeInOut(duration: 0.5))
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            //value 接收手势拖拽的位置
+                            self.viewState = value.translation
+                            self.show = true
+                        })
+                        .onEnded({ value in
+                            self.viewState = CGSize.zero
+                            self.show = false
+                        })
+                )
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("")
+        }
+        .frame(width: 340.0, height: 220.0)
+//        .background(Color.blue)
+//        .cornerRadius(10)
+//        .shadow(radius: 20)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .foregroundColor(.blue)
+                        .padding(.top)
+                    Text("Certificate")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                Image(systemName:"folder.fill")
+                    .resizable()
+                    .foregroundColor(.white)
+                    .frame(width:30, height: 30)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("community_banner_02")
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            Image("discovery_activity_coffee")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3)
+                .opacity(0.2)
+            
+            Text("In addition to the standard dynamic type sizes, the system offers a number of even larger sizes for users with accessibility needs. ")
+                .foregroundColor(.black)
+                .font(.subheadline)
+            Spacer()
+        }
+        .frame(minWidth:0 , maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600)
     }
 }
---
+++ DesignCode/DesignCode/Home.swift
@@ -0,0 +1,158 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by xueming liu on 2020/8/25.
+//  Copyright © 2020 xueming liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    @State var show: Bool = false
+    @State var showProfile: Bool = false
+    
+    var body: some View {
+        ZStack {
+            
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.spring())
+                .offset(y: showProfile ? 50 : UIScreen.main.bounds.height)
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 88)
+                .animation(.spring())
+            
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.spring())
+            
+            MenuView(show: $show)
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct MenuRow: View {
+    var image = ""
+    var text = ""
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color.black)
+                .frame(width: 30, height: 30)
+            Text(text)
+                .font(.headline)
+            Spacer()
+        }
+    }
+}
+
+struct Menu: Identifiable {
+    var id = UUID()
+    var title: String
+    var icon: String
+}
+
+let MenuData = [
+    Menu(title: "My Account", icon: "person.crop.circle"),
+    Menu(title: "Billing", icon: "creditcard"),
+    Menu(title: "Team", icon: "person.and.person"),
+    Menu(title: "sign out", icon: "arrow.uturn.down")
+]
+
+struct MenuView: View {
+    var menuAry = ["My Account", "Billing", "Team", "sign out"]
+    let menu = MenuData
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ForEach(menu) { item in
+                MenuRow(image: item.icon, text: item.title)
+            }
+            Spacer()
+        }
+        .padding(.top, 30)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0.0, y: 10.0, z: 0.0))
+        .offset(x: show ? 0: -UIScreen.main.bounds.width)
+        .animation(.default)
+        .onTapGesture {
+            self.show.toggle()
+        }
+    }
+}
+
+struct CircleButton: View {
+    var icon: String
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: .gray, radius: 10, x: 0, y: 10)
+    }
+}
+
+struct MenuButton: View {
+    @Binding var show : Bool
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Button(action: { self.show.toggle() }) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "list.dash")
+                        .foregroundColor(.black)
+                }
+                .padding(.trailing, 20)
+                .frame(width: 90, height: 60)
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(color: .gray, radius: 10, x: 0, y: 10)
+                
+            }
+            Spacer()
+                .padding(.leading, 20)
+        }
+    }
+}
+
+struct MenuRight: View {
+    @Binding var show : Bool
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            HStack {
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "person.crop.circle")
+                    
+                }
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "bell")
+                    
+                }
+            }
+            Spacer()
+                .padding(.leading, 20)
+        }
+    }
+}
